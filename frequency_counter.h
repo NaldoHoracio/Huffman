a247125f91c4f26d15164f82adeefd7fbe42ba74
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2016-09-25 16:59:55 -0300

    Create frequency_counter.h

---
+++ frequency_counter.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void count(char name_file[], unsigned int frequency[256]);
+
+void print_frequency(unsigned int frequency[256]);
+
+void frequency_path(char str[1024],unsigned int array[256]);
