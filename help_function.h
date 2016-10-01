9acdb75d9b73fca45037acc77ca64cc53d113ffc
NaldoHoracio <edvonaldo.horacio@hotmail.com>
2016-10-01 12:47:15 -0300

    Update help_function.h

--- help_function.h
+++ help_function.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int is_bit_i_set(unsigned char c, int i); // Verifica se o bit de posição i do caractere c está setado - tem valor 1
+int is_bit_i_set(unsigned char c, int i); // Protótipo da função que verifica se o bit de posição i do caractere c é 1 - ver help_function.c
 
-unsigned char set_bit(unsigned char c, int i); // Seta o bit da posição i do caractere c
+unsigned char set_bit(unsigned char c, int i); // Protótipo da função que seta o bit i do caractere c - ver help_function.h
 
